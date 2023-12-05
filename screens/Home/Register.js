cdb7969da134d87b6e55e5dd681d3b21920b93ba
Fouillen Mathis <mathis.fouillen@etudiant.univ-rennes1.fr>
2023-12-05 14:53:33 +0000

    fixed code

--- screens/Home/Register.js
+++ screens/Home/Register.js
@@ -12,38 +12,38 @@ const LoginScreen = () => {
   const handleRegister = () => {
     auth.currentUser.updateProfile({displayName: pseudo}).then(async () => {
 
-    // POST request to create user
-    var headers = new Headers();
-    headers.append("Content-Type", "application/json");
+      // POST request to create user
+      var headers = new Headers();
+      headers.append("Content-Type", "application/json");
 
-    let postOptions = {
-      method: 'POST',
-      headers: headers,
-      body: pseudo,
-    };
+      let postOptions = {
+        method: 'POST',
+        headers: headers,
+        body: pseudo,
+      };
 
-    await fetch('http://' + ipAddress + ':8080/user/create', postOptions)
-      .then(response => {
-          if(response.status == 409){
-              {{<p>Pseudo non avaiable</p>}} // TODO : beautify text
-          }
-          else if(response.ok){
-              console.log("user registered")
-          }
-          // TODO : do not navigate if response is not ok
-      })
-      .catch((error) => {
-          console.error(error);
-      })
+      await fetch('http://' + ipAddress + ':8080/user/create', postOptions)
+        .then(response => {
+            if(response.status == 409){
+                {{<p>Pseudo non avaiable</p>}} // TODO : beautify text
+            }
+            else if(response.ok){
+                console.log("user registered")
+            }
+            // TODO : do not navigate if response is not ok
+        })
+        .catch((error) => {
+            console.error(error);
+        })
 
-
-    auth.currentUser.updateProfile({
-      displayName: pseudo,
-      photoURL: "https://react.semantic-ui.com/images/avatar/small/jenny.jpg"
-    }).then(async () => {
-      navigation.replace("Tabs")
-    });
-  })
+      auth.currentUser.updateProfile({
+        displayName: pseudo,
+        photoURL: "https://react.semantic-ui.com/images/avatar/small/jenny.jpg"
+      }).then(() => {
+        navigation.replace("Tabs")
+      });
+    })
+  }
 
   return (
     <View
